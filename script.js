bebee28164b2f4c4726a6cacfd96b07a4083f073
thetamimahmed <wwwtamim000@gmail.com>
2022-03-25 14:40:49 +0600

    clear input field after add items , functionality added

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let count = 1;
 document.getElementById('add-btn').addEventListener('click', function(){
-    const inputField = document.getElementById('input-field').value;
+    let inputField = document.getElementById('input-field');
     const tableBody = document.getElementById('table-body');
     const createTr = document.createElement('tr');
     createTr.innerHTML  = `
     <tr>
         <th>${count++}</th>
-        <td>${inputField}</td>
+        <td>${inputField.value}</td>
         <td>
             <button class="btn btn-danger delete-btn">Delete</button>
             <button class="btn btn-success done-btn">Done</button>
@@ -14,4 +14,6 @@ document.getElementById('add-btn').addEventListener('click', function(){
     </tr>
   `;
     tableBody.appendChild(createTr);
+    inputField.value = '';
+    
 })
